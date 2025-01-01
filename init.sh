b84079823e74dea214d8130c1d940dfed757b5fe
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2025-01-01 18:18:10 +0900

    Use playbook for dotfiles

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install ansible
+ansible-playbook playbook-dotfiles.yml
