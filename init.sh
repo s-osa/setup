9502c8564ee9d9022a02b9793ebe500a6b0fb0a9
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2025-01-06 15:13:06 +0900

    Add inventory

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install ansible
-ansible-playbook /dev/stdin << "$(curl -s https://raw.githubusercontent.com/s-osa/setup/refs/heads/master/playbook-dotfiles.yml)"
+ansible-playbook -i localhost /dev/stdin << "$(curl -s https://raw.githubusercontent.com/s-osa/setup/refs/heads/master/playbook-dotfiles.yml)"
